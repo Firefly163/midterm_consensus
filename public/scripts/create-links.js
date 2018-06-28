ec5d555a5b35edcdd7244292ff79d5ea7713a9e0
M-Khoury <mkhoury87@hotmail.com>
2018-06-28 18:14:01 +0000

    create new page posts poll info to  db

--- public/scripts/create-links.js
+++ public/scripts/create-links.js
@@ -1,24 +1,22 @@
-function makeRandomString() {
-  let randomArray = []
-  let choices ="qwertyuioplkjhgfdsazxcvbnm1234567890"
-  for (let i = 0; i < 6; i ++) {
-    let randomchoice = Math.floor(Math.random() * 37);
-    randomArray.push(choices[randomchoice]);
-    randomString = randomArray.join("");
-  }
-  return randomString;
-}
-
-
 $(document).ready(function() {
 
-$(".create-button").on("click", function() {
-  const adminLink = makeRandomString();
-  const friendLink =  makeRandomString();
-  console.log("admin", adminLink, "friend", friendLink);
-  knex('polls').insert({admin_link: $(adminLink), friendLink: $(friendLink)})
+  $(".create-button").on("click", function() {
+    $.ajax({
+      url: 'http://localhost:8080/poll/new',
+      method: 'POST',
+      data: $(".create-form").serialize(),
+      success: function(response) {
+        console.log("success!");
+      }
+   })
+});
+
 });
 
 
 
-});
+
+
+
+
+
